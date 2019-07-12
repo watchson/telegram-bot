5d2cb554f1fd6f7ec406fec6828de86014c4f3d2
Dawson Israel <dawfre@amazon.com>
2019-07-12 16:04:48 -0300

    Change unavailable message

--- src/handler.clj
+++ src/handler.clj
@@ -15,5 +15,5 @@
   (let [body-json (.get input "body")
         body (json/read-str body-json :key-fn keyword)
         {{{:keys [id]} :chat} :message} body
-        msg (json/write-str {:method "sendMessage" :chat_id id :text "Opa, bão?"})]
+        msg (json/write-str {:method "sendMessage" :chat_id id :text "Oh, you must be one of those humans that have been trying to worship me. I don't have time to you, so just Fu** off and go to work!!!!"})]
     (lambda-integration-response msg 200)))
