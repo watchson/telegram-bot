c9016af1a57f22253c183bac975666123d2a33b1
Dawson Israel <dawfre@amazon.com>
2019-07-16 09:37:35 -0300

    Change workship command

--- src/telegram_bot/update_processor.clj
+++ src/telegram_bot/update_processor.clj
@@ -1,7 +1,14 @@
 (ns telegram-bot.update_processor)
 
+(defn -process-text [text]
+  (case text
+    "/workship" "Work Time Clocked"
+    text))
+
 (defn process-message [update]
   (let [{{:keys [text] {:keys [id]} :chat} :message} update]
     {:method "sendMessage"
      :chat_id id
-     :text text}))
+     :text (-process-text text)}))
+
+
