7a530235cfe748012fe16205c17993c1389bbb5a
Dawson Israel <dawfre@amazon.com>
2019-12-17 08:57:37 -0300

    Remove clojure

--- test/telegram_bot/update_processor_test.clj
+++
@@ -1,8 +0,0 @@
-(ns telegram-bot.update-processor-test
-  (:require [clojure.test :refer :all]
-            [telegram-bot.update_processor :refer :all]))
-
-(deftest a-test
-  (testing "Should return message"
-    (is (= {:method "sendMessage" :chat_id "666" :text "I do not understand your primitive language human, try another prayer!!!"}
-           (process-message {:message {:text "Hi" :chat {:id "666"} :from {:id "121212"}}})))))
