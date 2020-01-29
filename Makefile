e410951cc02a972ddd988f9d0d902d15c06b0978
Dawson Israel <dawfre@amazon.com>
2020-01-29 10:40:30 -0300

    Configure Lambda Rust

--- Makefile
+++ Makefile
@@ -1,16 +1,20 @@
 runtest:
-	lein test
+	cargo test
 
-build:
-	lein uberjar
+build-bin:
+	docker run --rm \
+		-v ${PWD}:/code \
+		-v ${HOME}/.cargo/registry:/root/.cargo/registry \
+		-v ${HOME}/.cargo/git:/root/.cargo/git \
+		softprops/lambda-rust
 
-release: runtest build
+release: build-bin
 
 package: release
 	sam package  --output-template-file packaged.yaml --s3-bucket watchson-telegram-bot-deploy-bucket
 
 deploy: package
-	sam deploy --template-file packaged.yaml --stack-name watchson-telegram-bot --capabilities CAPABILITY_IAM
+	sam deploy --template-file packaged.yaml --stack-name watchson-telegram-bot --capabilities CAPABILITY_NAMED_IAM
 
 setWebHook:
 	curl -F "url=$(shell aws cloudformation describe-stacks --stack-name watchson-telegram-bot --query 'Stacks[0].Outputs[?OutputKey==`TelegramBotApi`].OutputValue' --output text)" \
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use lambda_runtime::{error::HandlerError, lambda, Context};
+use serde_derive::{Deserialize, Serialize};
+use simple_logger;
+use std::collections::HashMap;
+use std::error::Error;
+
+#[derive(Deserialize)]
+struct ApiRequest {
+    #[serde(rename = "queryStringParameters")]
+    query_string_parameters: Option<HashMap<String, String>>,
+}
+
+#[derive(Serialize)]
+struct ApiResponse {
+    #[serde(rename = "statusCode")]
+    status_code: i32,
+    body: String,
+    #[serde(rename = "isBase64Encoded")]
+    is_base64_encoded: bool,
+    headers: HashMap<String, String>,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    simple_logger::init_with_level(log::Level::Debug)?;
+    lambda!(my_handler);
+
+    Ok(())
+}
+
+fn my_handler(request: ApiRequest, _c: Context) -> Result<ApiResponse, HandlerError> {
+    Ok(ApiResponse {
+        status_code: 200,
+        body: format!(
+            "{{\"response\": \"Welcome, {}!\" }}",
+            request
+                .query_string_parameters
+                .unwrap_or_default()
+                .get("name")
+                .unwrap_or(&"Stranger".to_string())
+        ),
+        is_base64_encoded: false,
+        headers: HashMap::new(),
+    })
 }
